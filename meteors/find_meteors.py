441859f2ff3c0144a7354a6f51f1eba259eeb4f5
juliobalu <45406856+juliobalu@users.noreply.github.com>
2020-05-17 17:22:36 +1000

    Adding a comment to explain __main__

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -16,7 +16,8 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
-    
+
+# __name__  is a way for your cade to tell if it was run as a python script or imported as a module. 
 if __name__ == '__main__':
     my_loc = (29.424122, -98.493628)
 
